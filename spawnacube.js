7ce13e1e4f49cbecfa6206fbe4c055531c7b162a
Camilla-Maskelyne <45618859+Camilla-Maskelyne@users.noreply.github.com>
2018-12-05 00:17:30 -0700

    Update spawnacube.js

--- spawnacube.js
+++ spawnacube.js
@@ -1,7 +1,7 @@
 var position = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
 var id = Entities.addEntity({
     position: position,
-    script: "https://github.com/Camilla-Maskelyne/HighFidelity/blob/master/makiversion2.js",
+    script: "https://raw.githubusercontent.com/Camilla-Maskelyne/HighFidelity/master/makiversion.js",
     type: "Box",
     name: "VorpalBox",
     color: {red: 0, green: 0, blue: 155}
